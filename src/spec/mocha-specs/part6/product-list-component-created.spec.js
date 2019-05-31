dcf36444fa6c6c59ea340bb1b5764a486038ed3d
rounak-kolya <rounak.kolya@tavant.com>
2019-05-31 10:21:51 +0530

    /src/spec/mocha-specs/part6/product-list-component-created.spec.js

--- src/spec/mocha-specs/part6/product-list-component-created.spec.js
+++ src/spec/mocha-specs/part6/product-list-component-created.spec.js
@@ -1,10 +1,37 @@
-const helpers = require("../helpers");
 
-describe("Project", () => {
-  it("The ProductListComponent doesn't exist - have you run the `ng` command to generate it yet?", () => {
-    helpers.readFile(
-      "src/app/product-list/product-list.component.ts",
-      "The ProductListComponent doesn't exist - have you run the `ng` command to generate it yet?"
-    );
+
+let fs = require('fs');
+
+let expect = require('chai').expect
+
+let assert = require('chai').assert
+
+
+
+describe('ProductListComponent', function() {
+
+  it(`should exist @product-list-component-created`,function () {
+
+    assert(fs.existsSync(__dirname + '/../../../app/product-list/product-list.component.ts'),
+
+        "The ProductListComponent doesn't exist - have you run the `ng` command to generate it yet?");
+
+
+
+    let file;
+
+    try {
+
+      file = fs.readFileSync(__dirname + '/../../../app/product-list/product-list.component.ts').toString();
+
+    } catch (e) {
+
+      assert(false, "The Product interface hasn't been created yet.")
+
+    }
+
+
+
   });
+
 });
